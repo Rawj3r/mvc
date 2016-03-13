27e2d8965dd28390aaac1bac2d47220d3ae64767
--replace-all <--replace-all>
2016-03-13 20:46:43 +0200

    controller index created

---
+++ controllers/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Nkosi RN
+ * Date: 3/13/16
+ * Time: 8:15 PM
+ */
+
+
+class Index{
+
+    function __construct(){
+        echo "We are in controller index";
+    }
+}
