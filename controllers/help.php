a71a9ba0ada3ac4f8df8060c882ba9e062592d45
--replace-all <--replace-all>
2016-03-13 20:45:14 +0200

    directories created

---
+++ controllers/help.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Nkosi RN
+ * Date: 3/13/16
+ * Time: 8:28 PM
+ */
+
+class Help{
+
+    function __construct(){
+        echo 'We are inside help';
+    }
+
+    public function move(){
+        echo "We are moving";
+    }
+
+
+}
